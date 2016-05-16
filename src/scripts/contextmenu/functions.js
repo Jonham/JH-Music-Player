1dc354ce0c17b0673cfd675b49209c9d8971d346
Jonham <jonham@qq.com>
2016-05-16 13:41:38 +0800

    fix: no more highlight error. Reset all line in ul#scrollLrc to item.className = "line";

--- src/scripts/contextmenu/functions.js
+++ src/scripts/contextmenu/functions.js
@@ -2,13 +2,19 @@ var contextMenuListener = function(e) {
     var lrc = loadedLRClist[0];
 	var timeline = lrc.timeTags;
 	var lrcList = lrc.lrc;
+    var ulLrc = $id('scrollLrc');
 
     if (e.target.tagName == "LI") {
         e.stopPropagation();
         e.preventDefault();
+
         var line = e.target.dataset.line;
-        console.log(line);
-        console.log(lrcList[line]);
         audio.currentTime = timeline[line] - 0.5;
+
+        // clear any 'focus' value in class
+        var domFocus = ulLrc.querySelectorAll('.focus');
+        for (var i = 0, len = domFocus.length; i < len; i++) {
+            domFocus[i].className = 'line';
+        }
     }
 };
