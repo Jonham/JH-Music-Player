ecde73d4846de6aab53b5e0ab682aa3fb4781620
Jonham <jonham@qq.com>
2016-05-25 19:43:44 +0800

    fix: cutting verbose dConsole.log words and add NS.audio
    1. shorten dConsole log messages
    2. add NS.audio for debuging objects on AudioContext

--- js/file-load.js
+++ js/file-load.js
@@ -2,29 +2,31 @@ var onFileLoad = function() {
     var dragEvent = function(e) {
         e.stopPropagation();
         e.preventDefault();
-        // console.log(e);
-        // jh = e;
     };
     var onFileDrop = function(e) {
         e.stopPropagation();
         e.preventDefault();
-        window.$.debugjh = e.dataTransfer.files[0];
-        onFileRead(e.dataTransfer.files[0], onFileDecode);
+        var filelist = e.dataTransfer.files;
+
+        _.each(filelist, function(value) {
+            onFileRead(value, onFileDecode);
+        });
     };
     var onFileSelect = function(dom) {
         if ( $.isDOMElement(dom) && dom.files ) {
-            onFileRead(dom.files[0], onFileDecode);
+            _.each(dom.files, function(value) {
+                onFileRead(value, onFileDecode);
+            });
         }
     };
     // file loading mode: 1:media; 2:lyric; 3:image
     var onFileRead = function(file, callback) {
-        dConsole.log('onFileRead');
         var fr = new FileReader();
 
         var type = file.type,
         size = file.size,
         name = file.name;
-        dConsole.log('is a file of ' + type + ' || size:' + size);
+        // dConsole.log('is a file of ' + type + ' || size:' + size);
 
         // test type support
         var loadingMode = false;
@@ -63,60 +65,99 @@ var onFileLoad = function() {
         if (type !== '') {
             var aMIME = type.split('/');
             typeMapByMIME( aMIME[0] );
-            dConsole.log('File MIME: ' + type + " || treat it as a " + aMIME[0] + ' file.');
+            // dConsole.log('File MIME: ' + type + " || treat it as a " + aMIME[0] + ' file.');
         } else {
         }
         // test twice
-        dConsole.log('File subfix: ' + subfix + " || treat it as a " + typeMapBySubfix(subfix));
+        // dConsole.log('File subfix: ' + subfix + " || treat it as a " + typeMapBySubfix(subfix));
 
         if (!loadingMode) { return false; }
 
         switch (loadingMode) {
             case 1: // media file
-            dConsole.log('FileReader: readAsArrayBuffer==>loading ' + file.name);
+            dConsole.log('FileReader: loading ' + file.name);
             fr.readAsArrayBuffer(file);
             break;
             case 2: // pure-text file Lyric
-            dConsole.log('FileReader: readAsText==>loading ' + file.name);
+            dConsole.log('FileReader: loading ' + file.name);
             fr.readAsText(file);
             break;
             case 3:
-            dConsole.log('FileReader: readAsDataURL==>loading ' + file.name);
+            dConsole.log('FileReader: loading ' + file.name);
             fr.readAsDataURL(file);
             break;
             default:
         }
 
         fr.onload = function(e) {
-            dConsole.log('FileReader: file.onload;');
+            // dConsole.log('FileReader: file loaded.');
             callback(fr.result, loadingMode);
+        };
+        fr.onerror = function(e) {
+            dConsole.log('ERROR: FileReader=>' + e);
         };
 
     };
+
+    /* audioLoader: NS.audio
+     ------------------------------------------------
+     *         songs => playlist, current one is in songs[0]
+     *         ctx   => AudioContext Instance
+     * bufferSources => ctx.createBufferSource() Instance ==> because their buffer can only set once
+     *
+    */
     var onFileDecode = function( fileBuffer, loadingMode ) {
         var audioLoader = function( fileBuffer ) {
-            if (!window.a) {
-                window.a = new AudioContext();
-                window.songs = [];
+            if (!NS.audio) {
+                NS.audio = {};
+                NS.audio.ctx = new AudioContext();
+                NS.audio.songs = [];
+                NS.audio.bufferSources = [];
             }
-            if (songs.length > 0) {
 
+            // let
+            var   ctx = NS.audio.ctx,
+                songs = NS.audio.songs,
+            bufferSrc = NS.audio.bufferSources;
+
+            songs.push(fileBuffer);
+
+            if (songs.length > 1) {
+                dConsole.log('audioLoader: song added to Playlist.');
+                return false;
             }
-            var src = a.createBufferSource();
-            src.connect(a.destination);
+            var addAudioBuffer = function(ctx, fileBuffer) {
+                // create a new audio buffer source
+                var src = ctx.createBufferSource();
+                src.connect(ctx.destination);
 
-            a.decodeAudioData(fileBuffer, function( audioBuffer ) {
-                src.buffer = audioBuffer;
-                dConsole.log('AudioContext: audio decode success');
-                src.start(0);
-                dConsole.log('AudioContext: start(0);');
-            });
+                NS.audio.bufferSources.push(src);
+
+                ctx.decodeAudioData(fileBuffer, function( audioBuffer ) {
+                    src.buffer = audioBuffer;
+                    // dConsole.log('AudioContext: audio decode success');
+                    src.start(0);
+                    dConsole.log('AudioContext: start playing;');
+                });
+
+                src.onended = function() {
+                    dConsole.log('last song ended.');
+                    var songs = NS.audio.songs;
+                    songs.shift();
+                    if (songs.length) {
+                        addAudioBuffer(ctx, songs[0]);
+                    }
+                }
+            };
+
+            addAudioBuffer(ctx, fileBuffer);
+
         };
         var lyricLoader = function( fileBuffer ) {
             dConsole.log(fileBuffer);
         };
         var imageLoader = function( fileBuffer ) {
-            dConsole.log('imageLoader: converting image.');
+            dConsole.log('imageLoader: set image as new background.');
             $wrap('#background').backgroundImage(fileBuffer);
         };
 
@@ -137,11 +178,11 @@ var onFileLoad = function() {
     var dragOrSelect = function() {
         var ug = navigator.userAgent;
         var result = ug.search(/windows|x11|Mac/i);
-        dConsole.log('dragOrSelect: ' + result +' || -1 for select');
+        dConsole.log(result === -1? 'Use input[type=file] to add files' : 'Drag&Drop files onto me!');
         return result !== -1; // return true if contains 'windows'
     };
     if (dragOrSelect() ) {
-        dConsole.log('support drag&drop');
+        // dConsole.log('support drag&drop');
         $on(document, 'dragstart', dragEvent);
         $on(document, 'dragenter', dragEvent);
         $on(document, 'dragover', dragEvent);
@@ -150,7 +191,7 @@ var onFileLoad = function() {
         $on(document, 'drop', onFileDrop);
     }
     else {
-        dConsole.log('use input[type=file] to load file.');
+        // dConsole.log('use input[type=file] to load file.');
         var fileInput = $dom('input');
             fileInput.type = 'file';
             fileInput.multiple = true;
