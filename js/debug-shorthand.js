abd3d444cf743d8ad307027f829c4731e45ccbcc
Jonham <jonham@qq.com>
2016-06-08 22:46:18 +0800

    func: add visualize.js to add visualizing effect to audio

--- js/debug-shorthand.js
+++ js/debug-shorthand.js
@@ -2,3 +2,8 @@ var list = NS.audio.songlist;
 list.init();
 NS.dom.menuSonglist.node.bind( list );
 var lyric = NS.lyric;
+
+var script = $dom('script');
+script.src = './js/visualize.js';
+document.body.appendChild(script);
+script.onload = function() { v(); };
---
+++ js/visualize.js
@@ -0,0 +1,54 @@
+var v = function( height, width, gain ) {
+
+    var ctx = NS.audio.ctx,
+        headGain = NS.audio.headGain;
+
+    var analyser = ctx.createAnalyser();
+    if (gain && gain.connect) {
+        gain.connect(analyser);
+    } else {
+        headGain.connect(analyser);
+    }
+
+    var canvas = $('#view-canvas'),
+        canvasCtx = canvas.getContext('2d');
+    var HEIGHT = canvas.height = height || 200;
+    var WIDTH  = canvas.width  = width  || 300;
+
+
+    analyser.fftSize = 512; // range: [32, 32768]
+    var bufferLength = analyser.frequencyBinCount;
+    console.log("bufferLength: " + bufferLength);
+
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+        drawVisual = requestAnimationFrame(draw);
+
+        analyser.getByteFrequencyData(dataArray);
+
+        // draw background
+        // canvasCtx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+        // canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+        var barWidth = (WIDTH / bufferLength) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        for (var i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+
+            canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',' + 20 + ',' + 20 +')';
+            // canvasCtx.fillRect(x, 0, barWidth, barHeight / 2);
+            canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+            x += barWidth + 1;
+        }
+    };
+
+    draw();
+}
